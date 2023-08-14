1853eb7417c4d9e1c127ea9e6ef00d17a5e820f0
Willslima <williansantos38@gmail.com>
2023-08-14 17:43:41 -0300

    adicionado getters e setters na classe

--- orientacao_a_objetos/conta.py
+++ orientacao_a_objetos/conta.py
@@ -1,32 +1,49 @@
 class Conta:
     def __init__(self,numero, titular, saldo, limite = 1000.0):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print('O seu saldo é R${}'.format(self.saldo))
+        print('O seu saldo é R${}'.format(self.__saldo))
 
-    def extrato_limite(self):
-        print('O seu limite é R${}'.format(self.limite))
+    def get_limite(self):
+        print('O seu limite é R${}'.format(self.__limite))
 
     def deposito(self, valor):
-        self.saldo += valor
-        print('Agora o seu saldo é {}'.format(self.saldo))
+        self.__saldo += valor
+        print('Agora o seu saldo é {}'.format(self.__saldo))
 
     def saque(self, valor):
-        self.saldo -= valor
-        print('Agora o seu saldo é {}'.format(self.saldo))
+        self.__saldo -= valor
+        print('Agora o seu saldo é {}'.format(self.__saldo))
 
+    def transferir(self, valor, destino):
+        self.saque(valor)
+        destino.deposito(valor)
+        
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+    
+    def set_limite(self, novo_limite):
+        self.__limite = novo_limite
+        return self.__limite
+    
 conta = Conta(1,'will',100)
-
-print(conta.titular)
+conta2 = Conta(2,'willian',100)
 
 conta.extrato()
+conta.transferir(50,conta2)
 
-conta.deposito(150)
+conta.extrato()
+conta2.extrato()
 
-conta.saque(50)
+print(conta.get_saldo())
+print(conta.get_titular())
 
-conta.extrato_limite()
+conta.set_limite(2500)
+conta.get_limite()
