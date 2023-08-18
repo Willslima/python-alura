6802cd599020893a51c3751eb6c05d5b7f2d36ee
Willslima <williansantos38@gmail.com>
2023-08-18 17:48:57 -0300

    herança built-in, vantagens e desvantagens de fazer herança, duck typing, uso de abcs

--- oo_avançado/modelo.py
+++ oo_avançado/modelo.py
@@ -1,21 +1,73 @@
 class Programa:
-    def __init__(self, nome, ano, like = 0):
+    def __init__(self, nome, ano):
         self._nome = nome.title()
         self.ano = ano
-        self._like = like
-    
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    def __str__(self):
+        return f'Nome: {self.nome} Likes: {self.likes}'
+
 class Filme(Programa):
-    def __init__(self, duracao):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
         self.duracao = duracao
+    
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
 
 class Serie(Programa):
-    def __init__(self, temporadas):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
         self.temporadas = temporadas
 
-la_casa_de_papel = Serie('la casa de papel',2017, 5)
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
-vingadores = Filme('vigadores - guerra infinita', 2018, 160)
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
 
-print(f'Nome: {vingadores.nome} - Duração: {vingadores.duracao} - Ano: {vingadores.ano}')
+    def __getitem__ (self, item):
+        return self._programas[item]
 
-print(f'Nome: {la_casa_de_papel.nome} - Temporadas: {la_casa_de_papel.temporadas} - Ano: {la_casa_de_papel.ano}')
+    @property
+    def listagem(self):
+        return self._programas
+
+    
+    def __len__(self):
+        return len(self._programas)
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+
+for i in range(100):
+    vingadores.dar_likes()
+
+for i in range(30):
+    atlanta.dar_likes()
+
+listinha = [atlanta, vingadores]
+
+playlist_fim_de_semana = Playlist('Fim de semana', listinha)
+
+print(len(playlist_fim_de_semana))
+
+for programa in playlist_fim_de_semana:
+    print(programa)
